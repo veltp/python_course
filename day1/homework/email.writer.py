0b4cedd903d72cf01861dd455ce25739ae256c97
veltp <veLTP@SR143124.sr.se>
2015-09-30 08:12:33 +0200

    färdig bonusuppgift
    som degtblev

--- day1/homework/email.writer.py
+++ day1/homework/email.writer.py
@@ -3,11 +3,7 @@
 Skriv en funktion som tar ett namn och en domän.
 Gör namnet mejlkompatibelt och skriv ut en fullständig e-postadress.
 """
-
-
-
-def emailify(name, domain):
-    """
+"""
      Koden ska göra följande:
      1) Ersätta mellanslag med punkter i namnet.
      2) Göra alla tecken till små bokstäver
@@ -15,19 +11,21 @@ def emailify(name, domain):
      i en e-postadress.
      4) Printa den kompletta adressen! 
     """
-    # Skriv din kod här!
-    print (name +"@"+ domain) 
+
+
+def emailify(name, domain):
     
-    if  "hur anger jag att name ska fatta nedanstående här behövs som jag ser en ifsats?"
-    small_name = name.lower()
-    small_english_name = name.lower ()
-    english_name = name.replace(" ",".")
-    english_name = name.replace("ö", "o")
-    english_name = name.replace("å", "a")
-    english_name = name.replace("ä", "a")
-    english_name = name.replace("é", "e")
-    """ nu kommer en fuling - vet inte hur jag anger dessa specialtecken"""
-    english_name = name.replace("阿部慎太郎", "asahi.co.jp")
+    """först måste man ange villkoren"""  
+     
+    name = name.lower()
+    name = name.replace(" ",".")
+    name = name.replace("ö", "o")
+    name = name.replace("å", "a")
+    name = name.replace("ä", "a")
+    name = name.replace("é", "e")
+    print (name +"@"+ domain) #här skrivs det ut
+  
+
 
 emailify("Annie Lööf", "riksdagen.se")
 emailify("David Lång", "riksdagen.se")
@@ -35,18 +33,4 @@ emailify("Emma Nohrén", "riksdagen.se")
 emailify("阿部慎太郎", "asahi.co.jp")
 emailify("Östen Rubin", "dn.se")
 
-""" Test av roboten"""
-write_emailify("Annie Lööf", "riksdagen.se")
-print("***************")
 
-write_emailify("David Lång", "riksdagen.se")
-print("***************")
-
-write_emailify("Emma Nohrén", "riksdagen.se")
-print("***************")
-
-write_emailify("阿部慎太郎", "asahi.co.jp")
-print("***************")
-
-write_emailify("Östen Rubin", "dn.se")
-print("***************")
